136775d336d3d70941ccdd5edaebf657cc80b87e
anyaga <nyaga.angel@outlook.com>
2025-09-22 21:13:26 -0500

    set up django properly

--- REU_Project/REU_Project/settings.py
+++ REU_Project/REU_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xp)y10vu--a61&%j25l#$##q9-+x=@n**9(6-f+0jzdt#6^o3d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] 
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'REU_Project',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'REU_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
