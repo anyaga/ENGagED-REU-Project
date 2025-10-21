5f1d72a6e0de2be0dc59683ab0b5a25ba43ad508
anyaga <nyaga.angel@outlook.com>
2025-10-21 03:23:34 -0500

    about modal open and close. need to change button shape

--- REU_Project/REU_Project/views.py
+++ REU_Project/REU_Project/views.py
@@ -3,38 +3,39 @@ from django.shortcuts import render
 
 
 
-def helper_side_panel(action):
-    if action == "open":
-        panel_size = "250px"
-    else:
-        panel_size = "0px"
+def helper_side_panel(request,action):
+    display_value = "none"
+    if "about-open" in request.POST:
+        display_value = "block"
+    if "about-close" in request.POST:
+        display_value = "none"
     context = {
-        "panel_size": panel_size,
+        "display_value":display_value,
         "action": action,
-
     }
     return context
 
 def home(request,action="close"):
-    context = helper_side_panel(action)
+    print("action" + str(action))
+    context = helper_side_panel(request,action)
     return render(request, "index.html",context)
 
 def abstract(request,action):
-    context = helper_side_panel(action)
+    context = helper_side_panel(request,action)
     return render(request,"abstract.html",context)
 
 def find(request,action):
-    context = helper_side_panel(action)
+    context = helper_side_panel(request,action)
     return render(request,"find.html",context)
 
 def reference(request,action):
-    context = helper_side_panel(action)
+    context = helper_side_panel(request,action)
     return render(request,"ref.html",context)
 
 def doc_preview(request,action):
-    context = helper_side_panel(action)
+    context = helper_side_panel(request,action)
     return render(request,"doc_preview.html",context)
 
 def participants(request,action):
-    context = helper_side_panel(action)
+    context = helper_side_panel(request,action)
     return render(request,"participants.html",context)
