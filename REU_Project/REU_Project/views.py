de4c37b935e39f511595fb96abe3e35cc8cacc2d
anyaga <nyaga.angel@outlook.com>
2025-10-30 19:57:55 -0500

    working version of the side panel

--- REU_Project/REU_Project/views.py
+++ REU_Project/REU_Project/views.py
@@ -4,14 +4,21 @@ from django.shortcuts import render
 
 
 def helper_side_panel(request,action):
+    print(request.POST)
     display_value = "none"
+    panel_size = "0em"
     if "about-open" in request.POST:
         display_value = "block"
     if "about-close" in request.POST:
         display_value = "none"
+    if 'table_of_contents' in request.POST:
+        panel_size = "20em"
+    if 'table_close' in request.POST:
+        panel_size = "0em"
     context = {
         "display_value":display_value,
         "action": action,
+        "panel_size":panel_size,
     }
     return context
 
